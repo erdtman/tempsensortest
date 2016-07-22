2e6341306600fb62c88ad1c171075b6308847088
Samuel Erdtman <erdtman@spotify.com>
2016-07-22 21:59:08 +0200

    responsive and correct current calue

--- models/Measurement.js
+++ models/Measurement.js
@@ -49,7 +49,7 @@ exports.now = function(id, start, end) {
   let collection = db.get().collection('measurement');
 
 //time : {$gte: new Date().getTime()-(10*60*1000) }
-  collection.find({id:id}).sort({"measurement":1}).limit(1).each(function(err, doc) {
+  collection.find({id:id}).sort({"time":-1}).limit(1).each(function(err, doc) {
     if (err) {
       return deferred.reject(new Error(err));
     }
