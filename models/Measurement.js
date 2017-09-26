4420808ef6d2d1c490c78dc6890c11bc52be7a06
Samuel Erdtman <erdtman@spotify.com>
2017-09-26 13:46:18 +0200

    fixed promies returns

--- models/Measurement.js
+++ models/Measurement.js
@@ -9,7 +9,8 @@ exports.create = function(id, value) {
   let deferred = Q.defer();
 
   if(parseFloat(value) < -50 || parseFloat(value) > 50) {
-    return deferred.reject(new Error("ignoring unresonable value"));
+    deferred.reject(new Error("ignoring unresonable value"));
+    return deferred.promise;
   }
 
   let measurement = {
@@ -21,7 +22,8 @@ exports.create = function(id, value) {
   let collection = db.get().collection('measurement');
   collection.save(measurement, function(err, doc) {
     if (err) {
-      return deferred.reject(new Error(err));
+      deferred.reject(new Error(err));
+      return deferred.promise;
     }
 
     deferred.resolve(doc);
@@ -74,7 +76,8 @@ exports.listAgregate = function(id, interval) {
     { "$sort" : {'_id' : 1}}
   ]).toArray(function(err, docs) {
     if (err) {
-      return deferred.reject(new Error(err));
+      deferred.reject(new Error(err));
+      return deferred.promise;
     }
     deferred.resolve(docs);
   });
@@ -87,7 +90,8 @@ exports.now = function(id, start, end) {
 
   collection.find({id:id}).sort({"time":-1}).limit(1).each(function(err, doc) {
     if (err) {
-      return deferred.reject(new Error(err));
+      deferred.reject(new Error(err));
+      return deferred.promise;
     }
 
     deferred.resolve(doc);
