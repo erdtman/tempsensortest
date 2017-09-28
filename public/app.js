d6c5d8795137e1581f39bdd297ebcadf1a666bca
Samuel Erdtman <erdtman@spotify.com>
2017-09-28 22:34:18 +0200

    display update

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 
 // http://www.chartjs.org
 
-function draw(labels, data, min, max) {
+function draw(labels, data, min, max, labels2, data2) {
   var ctx = $("#myChart");
   var myChart = new Chart(ctx, {
     type: 'line',
@@ -27,6 +27,26 @@ function draw(labels, data, min, max) {
             pointRadius: 1,
             pointHitRadius: 1,
             data: data,
+        },{
+            label: "innetemperatur",
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: "rgba(75,0,192,0.4)",
+            borderColor: "rgba(75,0,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,0,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,0,192,1)",
+            pointHoverBorderColor: "rgba(220,0,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 1,
+            data: data2,
         }]
     },
     options: {
