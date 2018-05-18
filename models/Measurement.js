8f9855136a6f7713864e5aa166da8932edd78a94
Samuel Erdtman <erdtman@spotify.com>
2018-05-18 22:42:33 +0200

    logging change

--- models/Measurement.js
+++ models/Measurement.js
@@ -19,11 +19,9 @@ exports.create = function(id, value) {
     "time": new Date().getTime()
   };
 
-
-
   let collection = db.get().collection('measurement');
   
-  collection.find({id:id}).sort({"time":1}).limit(1).each(function(err, doc) {
+  collection.find({id:id}).sort({"time":-1}).limit(1).each(function(err, doc) {
     if (err) {
       return deferred.reject(new Error(err));
     }
