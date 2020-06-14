d0ea2a977b7dc9629d4a114cbb1cdd53d1a8405b
Samuel Erdtman <erdtman@spotify.com>
2020-06-14 19:35:35 +0200

    initialized with new arduino folders

---
+++ arduino/power_meter/power_meter.ino
@@ -0,0 +1,12 @@
+#include "esp8266_setup/setup.h"
+
+void setup() {
+
+  // TODO give setup code the name and version of this code
+  // TODO maybe an if statement to verifiy that setup when well
+  parentSetup();
+}
+
+void loop() {
+  parentLoop();
+}
---
+++ arduino/timer/timer.ino
@@ -0,0 +1,9 @@
+#include "esp8266_setup/setup.h"
+
+void setup() {
+  parentSetup();
+}
+
+void loop() {
+  parentLoop();
+}
